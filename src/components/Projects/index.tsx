e7971a4ac2b7b75222446a027fe3d29456c0a7a2
enzoleao <enzopinheiro6@gmail.com>
2023-06-14 10:06:27 -0300

    adicionando nova timeline

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,16 +1,70 @@
 import styles from './Projects.module.scss'
-import Image from 'next/image'
-import AgendamentoImage from '../../../public/images/agendamento_pmpa.png'
-import ciapImage from '../../../public/images/ciap_pmpa.png'
-import Link from 'next/link'
-import { Timeline } from 'antd'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContextProvider } from '@/src/Context/useContext'
 import { useTranslation } from 'react-i18next'
+import { Timeline } from 'primereact/timeline'
+import { Card } from 'primereact/card'
+import sipmImage from '../../../public/images/agendamento_pmpa.png'
+import ciapImage from '../../../public/images/ciap_pmpa.png'
+
+import Image from 'next/image'
 export default function Projects() {
   const [showItems, setShowItems] = useState(false)
   const { setShowScroll } = useContextProvider()
   const { t } = useTranslation()
+  const events = [
+    {
+      status: t('projects.scheduling.name'),
+      date: '01/07/2022 - 01/03/2023',
+      color: '#fff',
+      image: sipmImage,
+      about: t('projects.scheduling.about'),
+      link: 'https://agendamentopmpa.com.br/',
+    },
+    {
+      status: t('projects.medicalReportManagment.name'),
+      date: '01/03/2023 - Now',
+      color: '#fff',
+      image: ciapImage,
+      about: t('projects.medicalReportManagment.about'),
+      link: 'https://ciap-front-end-project.vercel.app/',
+    },
+  ]
+  t('')
+  const customizedMarker = (item: any) => {
+    return (
+      <span
+        className="flex w-2rem h-2rem align-items-center justify-content-center text-white border-circle z-1 shadow-1"
+        style={{ backgroundColor: item.color }}
+      >
+        <i className={item.icon}></i>
+      </span>
+    )
+  }
+
+  const customizedContent = (item: any) => {
+    return (
+      <Card
+        className={styles.cardProjects}
+        title={
+          <a className="hover:text-blue-600" href={item.link}>
+            {item.status}
+          </a>
+        }
+        subTitle={item.date}
+      >
+        {item.image && (
+          <Image
+            src={item.image}
+            alt={item.name}
+            width={200}
+            className={styles.imageCard}
+          />
+        )}
+        <p className="text-justify">{item.about}</p>
+      </Card>
+    )
+  }
   useEffect(() => {
     const handleScroll = async () => {
       const timeline = document.getElementById('section2')
@@ -32,71 +86,14 @@ export default function Projects() {
         id="section2"
         className={`${styles.timeline} ${showItems && styles.show}`}
       >
-        <Timeline
-          mode="alternate"
-          style={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '80px',
-          }}
-          items={[
-            {
-              className: styles.timelineItem,
-              color: 'none',
-              children: (
-                <>
-                  <div className="flex w-full ">
-                    <Image
-                      src={AgendamentoImage}
-                      alt="teste"
-                      className={styles.projectsImages}
-                    />
-                  </div>
-                </>
-              ),
-              label: (
-                <div className={styles.testeste}>
-                  <h3>
-                    <Link href="https://agendamentopmpa.com.br">
-                      {t('projects.scheduling.name')}
-                    </Link>
-                  </h3>
-                  <h4>01/07/2022 - 01/03/2023</h4>
-                  <span className={styles.labelStyle}>
-                    <p>{t('projects.scheduling.about')}</p>
-                  </span>
-                </div>
-              ),
-            },
-            {
-              className: styles.timelineItem,
-              children: (
-                <>
-                  <Image
-                    className={styles.projectsImages}
-                    src={ciapImage}
-                    alt="teste"
-                  />
-                </>
-              ),
-              color: 'none',
-              label: (
-                <>
-                  <h3>
-                    <Link href="https://ciap-front-end-project.vercel.app">
-                      {t('projects.medicalReportManagment.name')}
-                    </Link>
-                  </h3>
-                  <h4>01/03/2023 - Now</h4>
-                  <span className={styles.labelStyleRight}>
-                    <p>{t('projects.medicalReportManagment.about')}</p>
-                  </span>
-                </>
-              ),
-            },
-          ]}
-        />
+        <div className="card">
+          <Timeline
+            value={events}
+            align="alternate"
+            marker={customizedMarker}
+            content={customizedContent}
+          />
+        </div>
       </div>
     </div>
   )
